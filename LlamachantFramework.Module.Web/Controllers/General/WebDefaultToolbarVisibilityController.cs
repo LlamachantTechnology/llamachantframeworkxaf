47fd2573d4a157bfd94875eb74b78206a706c02f
Dave Hesketh <dave@llamachant.com>
2018-03-31 12:22:51 -0400

    Framework Base Upload

---
+++ LlamachantFramework.Module.Web/Controllers/General/WebDefaultToolbarVisibilityController.cs
@@ -0,0 +1,40 @@
+﻿using DevExpress.ExpressApp.Web.Templates.ActionContainers;
+using LlamachantFramework.Module.Controllers.General;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.UI;
+
+namespace LlamachantFramework.Module.Web.Controllers.General
+{
+    public class WebDefaultToolbarVisibilityController : DefaultToolbarVisibilityController
+    {
+        protected override void SetToolbarVisibility(bool visible)
+        {
+            if (Frame.Template != null)
+            {
+                foreach (ActionContainerHolder holder in FindActionHolders((Control)Frame.Template))
+                {
+                    string current = holder.Style[HtmlTextWriterStyle.Display];
+                    holder.Style[HtmlTextWriterStyle.Visibility] = visible ? current : "Hidden";
+                    holder.Parent.Visible = visible;
+                }
+            }
+        }
+
+        private List<ActionContainerHolder> FindActionHolders(Control c)
+        {
+            List<ActionContainerHolder> holders = new List<ActionContainerHolder>();
+            foreach (Control control in c.Controls)
+            {
+                if (control is ActionContainerHolder)
+                    holders.Add((ActionContainerHolder)control);
+                holders.AddRange(FindActionHolders(control));
+            }
+
+            return holders;
+        }
+    }
+}
---
+++ LlamachantFramework.Module.Web/Controllers/General/WebHighlightTabsController.cs
@@ -0,0 +1,106 @@
+﻿using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Model;
+using DevExpress.ExpressApp.Web.Layout;
+using DevExpress.Persistent.Base;
+using DevExpress.Web;
+using LlamachantFramework.Module.Controllers.General;
+using LlamachantFramework.Module.Interfaces.Model;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.UI;
+
+namespace LlamachantFramework.Module.Web.Controllers.General
+{
+    public class WebHighlightTabsController : ViewController<DetailView>
+    {
+        private List<ASPxTabControlBase> tabControls = new List<ASPxTabControlBase>();
+
+        public WebHighlightTabsController() { }
+
+        internal WebLayoutManager LayoutManager { get { return View.LayoutManager as WebLayoutManager; } }
+        public IModelHighlightOptions HighlightOptions { get { return Application.Model.Options as IModelHighlightOptions; } }
+
+        protected override void OnActivated()
+        {
+            base.OnActivated();
+
+            if (HighlightOptions.ShowCountsInTabs)
+                LayoutManager.ItemCreated += LayoutManager_ItemCreated;
+        }
+
+        protected override void OnDeactivated()
+        {
+            if (HighlightOptions.ShowCountsInTabs)
+                LayoutManager.ItemCreated -= LayoutManager_ItemCreated;
+
+            base.OnDeactivated();
+        }
+
+        private void LayoutManager_ItemCreated(object sender, ItemCreatedEventArgs e)
+        {
+            if (e.ModelLayoutElement is IModelTabbedGroup && e.TemplateContainer != null)
+                e.TemplateContainer.Load += TemplateContainer_Load;
+        }
+
+        private void TemplateContainer_Load(object sender, EventArgs e)
+        {
+            Control c = (Control)sender;
+            c.Load -= TemplateContainer_Load;
+            ASPxTabControlBase tabcontrol = FindFirstControl<ASPxTabControlBase>(c);
+            if (tabcontrol != null)
+            {
+                tabControls.Add(tabcontrol);
+                CustomizeASPxTabControl(tabcontrol);
+            }
+        }
+
+        private T FindFirstControl<T>(Control parent) where T : Control
+        {
+            foreach (Control control in parent.Controls)
+            {
+                if (control is T)
+                    return (T)control;
+            }
+            foreach (Control control in parent.Controls)
+                return FindFirstControl<T>(control);
+            return null;
+        }
+
+        private void CustomizeASPxTabControl(ASPxTabControlBase tabControl)
+        {
+            ASPxPageControl pagecontrol = tabControl as ASPxPageControl;
+            if (pagecontrol != null)
+            {
+                foreach (TabPage page in pagecontrol.TabPages)
+                {
+                    string propertyname = page.Name;
+                    if (propertyname.Contains("_Group"))
+                        propertyname = propertyname.Substring(0, propertyname.IndexOf("_Group"));
+
+                    MemberInfo[] members = this.View.CurrentObject.GetType().GetMember(propertyname);
+                    if (members.Length > 0)
+                    {
+                        object obj = XafTypesInfo.Instance.FindTypeInfo(this.View.CurrentObject.GetType()).FindMember(propertyname).GetValue(this.View.CurrentObject);
+                        if (obj is ICollection)
+                        {
+                            if ((obj as ICollection).Count > 0)
+                            {
+                                page.Text = String.Format("{0} ({1})", this.View.FindItem(propertyname).Caption, (obj as ICollection).Count);
+                                page.TabStyle.Font.Bold = HighlightOptions.BoldTabsWithCounts;
+                            }
+                        }
+                        else if (obj != null)
+                        {
+                            page.TabStyle.Font.Bold = HighlightOptions.BoldTabsWithCounts;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
